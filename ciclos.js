81fd45d72057d2906f3c838491a2fcc0c6706ace
Brandon0311 <enerobrandon11@gmail.com>
2019-10-10 18:45:30 -0500

    Empezando

---
+++ ciclos.js
@@ -0,0 +1,59 @@
+// 1.
+// var n;
+// for ( n = 1; n < 21; n++) {
+//     document.write("Número"+n+"<br>")
+    
+// }
+
+
+//  2. (no comprendo)
+//  for (var n1 = 0; n1 < 31; n1++) {
+//      document.write(n1+"<br>")
+//  }
+
+
+// 3.
+// var y=7;
+
+// for (var x = 1; x < 13; x++) {
+//     document.write(x," x ",y," = ",x*y,"<br>");   
+// }
+  
+
+// 4.
+
+// for (var i = 1; i <= 50; i++) {
+//     if (i % 2 == 0)
+    
+//   {
+
+//     document.write(i," Es Par <br>");
+
+//   }
+
+//   else {document.write(i," Es Impar <br>");}
+// }
+
+5.
+for (let i = 1; i <= 100; i++) {
+    
+    if (i % 3 == 0) {
+        document.write(i, " pin <br>")
+    } else if (i % 5 == 0) {
+        document.write(i, " pon <br>");
+    } else{
+        document.write("pin pon  <br>");
+    }
+}  
+
+
+
+
+
+
+
+
+
+
+
+ 
